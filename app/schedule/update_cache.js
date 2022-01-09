accc7f3d04b6b8ef78cf171bbb4566a02adcaed8
七彩 <wangriqiao@cai-inc.com>
2022-01-09 17:51:47 +0800

    feat: 修改定时任务config

--- app/schedule/update_cache.js
+++ app/schedule/update_cache.js
@@ -5,9 +5,7 @@ class UpdateCache extends Subscription {
   // 通过 schedule 属性来设置定时任务的执行间隔等配置
   static get schedule() {
     return {
-      cron: '0 30 7 * * *', // 每天的7点30分0秒执行
-      // interval: '1m', // 1 分钟间隔
-      type: 'all', // 指定所有的 worker 都需要执行
+      cron: '0 30 9 * * *', // 每天的7点30分0秒执行
     };
   }
 
